6ba83941dd1f2e8c954eb1bfe4e3997243604e25
Hamidreza Hassanzadeh <hamidrezahz1999@gmail.com>
2025-09-02 17:07:18 +0330

    added constants

---
+++ constants/routes.js
@@ -0,0 +1,41 @@
+const routes = {
+auth: "/auth",
+
+dashboard: "/dashboard",
+
+admins: "/dashboard/admins",
+
+products: "/dashboard/products",
+
+createProduct: "/dashboard/products/create",
+
+categories: "/dashboard/categories",
+
+createCategory: "/dashboard/categories/create",
+
+brands: "/dashboard/brands",
+
+createBrand: "/dashboard/brands/create",
+
+orders: "/dashboard/orders",
+
+cart: "/dashboard/cart",
+
+reviews: "/dashboard/reviews",
+
+generalSettings: "/dashboard/general-settings",
+
+campaigns: "/dashboard/campaigns",
+
+coupons: "/dashboard/coupons",
+
+transactions: "/dashboard/transactions",
+
+contactForms: "/dashboard/contact-forms",
+
+blog: "/dashboard/blog",
+
+createBlog: "/dashboard/blog/create",
+}
+
+export default routes
